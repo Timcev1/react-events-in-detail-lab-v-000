8b1f92c264a84133efc15d4316f3df9fb6eefb2d
Timothy Cevallos <timcev@gmail.com>
2017-10-13 05:06:22 +0000

    automatically backed up by learn

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -5,7 +5,7 @@ class CoordinatesButton extends React.Component {
   click = (event) => {
     this.props.onReceiveCoordinates({event.clientX, event.clientY});
   }
-  
+
   render(){
     return (
       <button onClick = {this.click}> Coordinates </button>
